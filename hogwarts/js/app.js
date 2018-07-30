79d441f76c9089ad97ecd4059daf22a506d20dab
Laurie Carano <lauriecarano@gmail.com>
2018-07-30 09:36:01 -0600

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -12,6 +12,11 @@ $(() => {
 let $container = $('#container');
 
 	$("<h1>Hogwarts</h1>").appendTo("#container");
+	$("<h2>Laurie</h2>").appendTo("#container");
+	$("<h3>Gryffindor</h3>").appendTo("#container");
+	$("<h4 class='owl'>owlName</h4>").appendTo("#container");
+	$("<h4>wandName</h4>").appendTo("#container");
+
 });
 
 
